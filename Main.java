c60198a7d835de1178ee5a208c718cc60c568937
tako2039 <takeo2039@gmail.com>
2022-04-05 18:43:19 -0500

    finish practicing input

--- Main.java
+++ Main.java
@@ -1,8 +1,31 @@
 package com.timbuchalka;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+        // Prompt user
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your year of birth: ");
+        boolean hasNextInt = scanner.hasNextInt();
+
+        if(hasNextInt) {
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.println("Enter your name: ");
+            String name = scanner.nextLine();
+
+            int age = 2022 - yearOfBirth;
+            if(age >= 0 && age <= 120) {
+                System.out.println("Your name is " + name + ", and you're " + age + " years old.");
+            } else {
+                System.out.println("Invalid year of birth.");
+            }
+        } else {
+            System.out.println("Unable to parse year of birth.");
+        }
+
+        scanner.close();
     }
 }
